5bebed79364aaba9c53bde699f70b99b81149829
buyna-gtn <b.bekhbayar@gtn.co.jp>
2024-12-23 00:06:02 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Buyantogtokh Bekhbayar on 2024.12.23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
